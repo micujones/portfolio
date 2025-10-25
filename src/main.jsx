7ec0857e424ea5e0776acd91ceb4d4b2506c54d7
Mike Jones, II <micujonesii@gmail.com>
2025-10-25 00:14:18 -0400

    Configure for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { App } from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename="/portfolio">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
         {/* <HashRouter basename="/portfolio">
